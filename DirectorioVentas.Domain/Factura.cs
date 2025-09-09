471cb2c184d8bc690dc5aac7cf868ac804c9b6e1
germanmbueno <germanmbueno@gmail,com>
2025-09-09 15:29:54 -0600

    feat: add initial domain entities Persona and Factura

---
+++ DirectorioVentas.Domain/Factura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectorioVentas.Domain
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto { get; set; }
+
+        public int PersonaId { get; set; }
+        public Persona Persona { get; set; } = default!;
+    }
+}
---
+++ DirectorioVentas.Domain/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace DirectorioVentas.Domain
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = default!;
+        public string ApellidoPaterno { get; set; } = default!;
+        public string? ApellidoMaterno { get; set; }
+        public string Identificacion { get; set; } = default!;
+
+        public List<Factura> Facturas { get; set; } = new();
+    }
+}
